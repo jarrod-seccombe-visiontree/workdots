063db0857f630926299c95fd53db330759707d21
Jarrod Seccombe <jseccombe@visiontree.com>
2023-05-31 16:22:36 -0400

    [nvim] add smooth scrolling

--- neovim/.config/nvim/lua/plugins/editing.lua
+++ neovim/.config/nvim/lua/plugins/editing.lua
@@ -21,6 +21,12 @@ return {
         end,
     },
     {
+        'karb94/neoscroll.nvim',
+        config = function()
+            require('neoscroll').setup()
+        end
+    },
+    {
         'folke/todo-comments.nvim',
         opts = {
             highlight = {
